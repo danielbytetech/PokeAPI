93ea0c8d040d2ea920c482488d74d5f007eb04ab
danib <danibakugan1@gmail.com>
2024-12-12 15:21:47 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using System;
+
+namespace PokeAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://pokeapi.co/api/v2/pokemon/");
+            var request = new RestRequest("", Method.Get);
+            var reponse = client.Execute(request);
+
+            if (reponse.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine(reponse.Content);
+            }
+            else
+            {
+                Console.WriteLine(reponse.ErrorMessage);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
