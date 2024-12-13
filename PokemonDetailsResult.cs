9ce9a022123262465dea4ddc8830b6fd6fc7ebfe
danib <danibakugan1@gmail.com>
2024-12-13 15:53:57 -0300

    Implementei uma interface para escolha do Pokemon e os detalhes

---
+++ PokemonDetailsResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Tamagotchi
+{
+    internal class PokemonDetailsResult
+    {
+        public List<AbilityDetail> Abilities { get; set; }
+        public string Name { get; set; }
+        public string Order { get; set; }
+        public string Height { get; set; }
+        public string Weight { get; set; }
+    }
+
+    public class AbilityDetail
+    {
+        public Ability Ability { get; set; }
+        public bool IsHidden { get; set; }
+        public int Slot { get; set; }
+    }
+
+    public class Ability
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,23 +1,72 @@
-﻿using RestSharp;
+﻿using Newtonsoft.Json;
+using RestSharp;
 using System;
 
-namespace PokeAPI
+namespace Tamagotchi
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var client = new RestClient("https://pokeapi.co/api/v2/pokemon/");
+            //Obter a lista de especies de Pokémon
+            var client = new RestClient("https://pokeapi.co/api/v2/pokemon-species/");
             var request = new RestRequest("", Method.Get);
-            var reponse = client.Execute(request);
+            var response = client.Execute(request);
 
-            if (reponse.StatusCode == System.Net.HttpStatusCode.OK)
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
-                Console.WriteLine(reponse.Content);
+                var pokemonEspeciesResposta = JsonConvert.DeserializeObject<PokemonSpeciesResult>(response.Content);
+
+                //Apresentar as opções ao jogador
+                Console.WriteLine("Escolha um Tamagotchi:");
+                for(int i = 0; i < pokemonEspeciesResposta.Results.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {pokemonEspeciesResposta.Results[i].Name}");
+                }
+
+                // Obter a escolha do jogador
+                int escolha;
+
+                while (true)
+                {
+                    Console.WriteLine("\n");
+                    Console.Write("Escolha um número: ");
+                    if(!int.TryParse(Console.ReadLine(), out escolha) && escolha >= 1 && escolha <= pokemonEspeciesResposta.Results.Count)
+                    {
+                        Console.WriteLine("Escolha inválida. Tente novamente.");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                //Obter as caracteriscas do Pokémon escolhido
+                client = new RestClient($"https://pokeapi.co/api/v2/pokemon/{escolha}");
+                request = new RestRequest("", Method.Get);
+                response = client.Execute(request);
+
+                var pokemonDetalhes = JsonConvert.DeserializeObject<PokemonDetailsResult>(response.Content);
+
+                var pokemonEscolhido = pokemonEspeciesResposta.Results[escolha - 1];
+
+                Console.WriteLine("\n");
+                Console.WriteLine($"Você escolheu o {pokemonEscolhido.Name}");
+                Console.WriteLine("Detalhes:");
+                Console.WriteLine($"- Nome: {pokemonDetalhes.Name}");
+                Console.WriteLine($"- Peso: {pokemonDetalhes.Weight}");
+                Console.WriteLine($"- Altura: {pokemonDetalhes.Height}");
+
+                Console.WriteLine("\n Habilidades do Mascote: ");
+
+                foreach ( var abilityDetail in pokemonDetalhes.Abilities)
+                {
+                    Console.WriteLine($"Nome da Habilidade: {abilityDetail.Ability.Name}");
+                }
             }
             else
             {
-                Console.WriteLine(reponse.ErrorMessage);
+                Console.WriteLine(response.ErrorMessage);
             }
             Console.ReadKey();
 
